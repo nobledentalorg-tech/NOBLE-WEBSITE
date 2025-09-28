55d290cfbd2292f5c3d89f9d9a08175a97e52123
Nobledental <dr.dhivakaran@nobledentalnallagandla.in>
2025-09-29 04:50:58 +0530

    Create navbar.php

---
+++ Dental-Case-Manager/includes/navbar.php
@@ -0,0 +1,7 @@
+<nav class="navbar navbar-light bg-white border-bottom shadow-sm">
+  <div class="container-fluid">
+    <span class="navbar-text fw-semibold">
+      <?= $page_title ?? 'Dashboard' ?>
+    </span>
+  </div>
+</nav>
